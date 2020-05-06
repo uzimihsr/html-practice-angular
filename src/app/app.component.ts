73bf8e57d5c5e7c66e0db0bd9da42edecc1e8e6e
uzimihsr <uzimihsr@gmail.com>
2020-05-06 18:25:58 +0900

    first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'html-practice-angular';
+  title = 'HTML practice page (Angular)';
 }
